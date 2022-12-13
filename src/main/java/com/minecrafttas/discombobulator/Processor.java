715bd09f079600d25da07154a815770e23c6143b
Scribble <scribble@minecrafttas.com>
2022-12-13 21:38:41 +0100

    Set up unit testing

---
+++ src/main/java/com/minecrafttas/discombobulator/Processor.java
@@ -0,0 +1,9 @@
+package com.minecrafttas.discombobulator;
+
+import java.util.List;
+
+public class Processor {
+	public static List<String> preprocess(String version, List<String> lines){
+		return null;
+	}
+}
---
+++ src/test/java/com/minecrafttas/discombobulator/ProcessorTest.java
@@ -0,0 +1,14 @@
+package com.minecrafttas.discombobulator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProcessorTest {
+
+	@Test
+	void testPreprocess() {
+		// Yes we will do unit testing on this
+	}
+
+}
