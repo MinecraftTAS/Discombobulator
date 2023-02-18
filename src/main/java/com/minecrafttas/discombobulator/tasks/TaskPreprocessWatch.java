82fe4f650d30bb4bdd4945a26b66e97ce6a67fca
Scribble <scribble@minecrafttas.com>
2023-02-18 17:46:19 +0100

    Fixed FileWatcher Threads not closing when closing the main thread

--- src/main/java/com/minecrafttas/discombobulator/tasks/TaskPreprocessWatch.java
+++ src/main/java/com/minecrafttas/discombobulator/tasks/TaskPreprocessWatch.java
@@ -2,6 +2,7 @@ package com.minecrafttas.discombobulator.tasks;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.ClosedWatchServiceException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
@@ -20,11 +21,12 @@ import com.minecrafttas.discombobulator.utils.SocketLock;
 
 /**
  * This task preprocesses the source code on file change
+ * 
  * @author Pancake
  */
 public class TaskPreprocessWatch extends DefaultTask {
 
-	private List<Thread> threads = new ArrayList<>();
+	private List<FileWatcherThread> threads = new ArrayList<>();
 
 	@TaskAction
 	public void preprocessWatch() {
@@ -48,21 +50,21 @@ public class TaskPreprocessWatch extends DefaultTask {
 		// Prepare list of physical version folders
 		// Left=Version, Right=Path to gradle folder
 		List<Pair<String, String>> versionsConfig = Discombobulator.getVersionPairs();
-		
+
 		List<Pair<String, Path>> versions = new ArrayList<>();
-		
+
 		for (Pair<String, String> versionConf : versionsConfig) {
 			String path = versionConf.right();
-			if(path == null) {
+			if (path == null) {
 				path = versionConf.left();
 			}
 			File rootDir = new File(this.getProject().getProjectDir() + File.separator + path);
-			if(new File(rootDir, "build.gradle").exists()) {
+			if (new File(rootDir, "build.gradle").exists()) {
 				String ver = versionConf.left();
 				versions.add(Pair.of(ver, new File(rootDir, "src").toPath().toAbsolutePath()));
 			}
 		}
-		
+
 		for (Pair<String, Path> version : versions)
 			this.watch(version.right(), versions);
 
@@ -73,105 +75,137 @@ public class TaskPreprocessWatch extends DefaultTask {
 		} catch (IOException e) {
 			// don't care
 		}
-		for (Thread thread : this.threads)
-			thread.interrupt();
+		for (FileWatcherThread thread : this.threads)
+			thread.close();
 		lock.unlock();
 	}
 
 	/**
 	 * Watches and preproceses a source folder
-	 * @param file Source folder
+	 * 
+	 * @param file     Source folder
 	 * @param versions Map of versions
 	 */
 	private void watch(Path file, List<Pair<String, Path>> versions) {
-		Thread thread = new Thread(() -> {
-			String version = file.getParent().getFileName().toString();
-			FileWatcher watcher = null;
+		String version = file.getParent().getFileName().toString();
+		FileWatcher watcher = null;
+		try {
+			watcher = constructFileWatcher(file, versions, version);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		threads.add(new FileWatcherThread(watcher, version));
+	}
+
+	private FileWatcher constructFileWatcher(Path file, List<Pair<String, Path>> versions, String version) throws IOException {
+		return new FileWatcher(file) {
+
+			@Override
+			protected void onNewFile(Path path) {
+
+			}
+
+			@Override
+			protected void onModifyFile(Path path) {
+				// Get the filename that is getting prerprocessed
+				String filename = path.getFileName().toString();
+
+				PathLock schedule = Discombobulator.pathLock;
+				if (schedule.isLocked(path))
+					return;
+
+				// Get path relative to the root dir
+				Path relativeFile = file.relativize(path);
+				try {
+					// Modify this file in other versions too
+
+					// Read the original file
+					List<String> inLines = Files.readAllLines(path);
+
+					// Iterate through all versions
+					for (Pair<String, Path> subVersion : versions) {
+						// If the version equals the original version, then skip it
+						if (subVersion.right().equals(file)) {
+							continue;
+						}
+
+						// Preprocess the lines
+						List<String> lines = Discombobulator.processor.preprocess(subVersion.left(), inLines, filename);
+
+						// Write file
+						Path outFile = subVersion.right().resolve(relativeFile);
+
+						schedule.scheduleAndLock(outFile);
+						Files.createDirectories(outFile.getParent());
+						SafeFileOperations.write(outFile, lines, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
+						Files.setLastModifiedTime(outFile, Files.getLastModifiedTime(path));
+					}
+					// Modify this file in base project
+					List<String> lines = Discombobulator.processor.preprocess(null, inLines, filename);
+					Path outFile = new File(TaskPreprocessWatch.this.getProject().getProjectDir(), "src").toPath().toAbsolutePath().resolve(relativeFile);
+					Files.createDirectories(outFile.getParent());
+					SafeFileOperations.write(outFile, lines, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
+					Files.setLastModifiedTime(outFile, Files.getLastModifiedTime(path));
+					System.out.println(String.format("Processed %s in %s", path.getFileName(), version));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			@Override
+			protected void onDeleteFile(Path path) {
+				var relativeFile = file.relativize(path);
+				// Delete this file in other versions too
+				// Iterate through all versions
+				for (Pair<String, Path> subVersion : versions) {
+					if (subVersion.right().equals(file))
+						continue;
+					SafeFileOperations.delete(subVersion.right().resolve(relativeFile).toFile());
+				}
+				// Delete this file in base project
+				SafeFileOperations.delete(new File(TaskPreprocessWatch.this.getProject().getProjectDir(), "src").toPath().toAbsolutePath().resolve(relativeFile).toFile());
+			}
+		};
+	}
+
+	/**
+	 * Custom closable FileWatcher Thread
+	 * <p>Previously the threads kept running in the background, even after the main thread closed. With this, we can close the threads for good.
+	 * @author Scribble
+	 *
+	 */
+	private class FileWatcherThread extends Thread {
+
+		private FileWatcher watcher;
+
+		public FileWatcherThread(FileWatcher watcher, String version) {
+			super("FileWatcher-" + version);
+			System.out.println("Started watching "+version);
+			this.watcher = watcher;
+			this.setDaemon(true);
+			this.start();
+		}
+
+		@Override
+		public void run() {
 			try {
-				
-				watcher = new FileWatcher(file) {
-
-					@Override
-					protected void onNewFile(Path path) {
-
-					}
-
-					@Override
-					protected void onModifyFile(Path path) {
-						// Get the filename that is getting prerprocessed
-						String filename = path.getFileName().toString();
-
-						PathLock schedule = Discombobulator.pathLock;
-						if (schedule.isLocked(path))
-							return;
-
-						// Get path relative to the root dir
-						Path relativeFile = file.relativize(path);
-						try {
-							// Modify this file in other versions too
-							
-							// Read the original file
-							List<String> inLines = Files.readAllLines(path);
-							
-							// Iterate through all versions
-							for (Pair<String, Path> subVersion : versions) {
-								// If the version equals the original version, then skip it
-								if (subVersion.left().equals(version)) {
-									continue;
-								}
-								System.out.println(subVersion.left()+" "+version);
-								
-								// Preprocess the lines
-								List<String> lines = Discombobulator.processor.preprocess(subVersion.left(), inLines, filename);
-								
-								
-								// Write file
-								Path outFile = subVersion.right().resolve(relativeFile);
-								
-								schedule.scheduleAndLock(outFile);
-								Files.createDirectories(outFile.getParent());
-								SafeFileOperations.write(outFile, lines, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
-								Files.setLastModifiedTime(outFile, Files.getLastModifiedTime(path));
-							}
-							// Modify this file in base project
-							List<String> lines = Discombobulator.processor.preprocess(null, inLines, filename);
-							Path outFile = new File(TaskPreprocessWatch.this.getProject().getProjectDir(), "src").toPath().toAbsolutePath().resolve(relativeFile);
-							Files.createDirectories(outFile.getParent());
-							SafeFileOperations.write(outFile, lines, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
-							Files.setLastModifiedTime(outFile, Files.getLastModifiedTime(path));
-							System.out.println("Processed " + path.getFileName());
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-
-					@Override
-					protected void onDeleteFile(Path path) {
-						var relativeFile = file.relativize(path);
-						// Delete this file in other versions too
-						// Iterate through all versions
-						for (Pair<String, Path> subVersion : versions) {
-							if (subVersion.left().equals(version))
-								continue;
-							SafeFileOperations.delete(subVersion.right().resolve(relativeFile).toFile());
-						}
-						// Delete this file in base project
-						SafeFileOperations.delete(new File(TaskPreprocessWatch.this.getProject().getProjectDir(), "src").toPath().toAbsolutePath().resolve(relativeFile).toFile());
-						System.out.println("Deleted " + path.getFileName());
-					}
-				};
 				watcher.watch();
 			} catch (IOException e) {
 				e.printStackTrace();
 			} catch (InterruptedException e) {
-				System.out.println("Shutting down FileWatcher for " + version);
+				System.out.println("Interrupting "+this.getName());
 				if (watcher != null)
 					watcher.close();
+				e.printStackTrace();
+			} catch (ClosedWatchServiceException e) {
+				System.out.println("Shutting down "+this.getName());
 			}
-		});
-		thread.setName("FileWatcher-" + file.getFileName());
-		thread.setDaemon(true);
-		thread.start();
+		}
+		
+		public void close() {
+			if(watcher!=null)
+				watcher.close();
+		}
 	}
 
 }
