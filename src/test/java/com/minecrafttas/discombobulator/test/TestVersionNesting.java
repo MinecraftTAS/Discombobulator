3f14e73fcd0b44f6328022208db90ef83753aadf
Scribble <scribble@minecrafttas.com>
2023-02-22 23:58:51 +0100

    Tests for nesting
    - Tested up to 3 nesting statements

--- src/test/java/com/minecrafttas/discombobulator/test/TestVersionNesting.java
+++ src/test/java/com/minecrafttas/discombobulator/test/TestVersionNesting.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.gradle.internal.impldep.org.apache.commons.compress.utils.FileNameUtils;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import com.minecrafttas.discombobulator.Processor;
@@ -39,7 +38,7 @@ class TestVersionNesting extends TestBase {
 	 */
 	@Test
 	void testTargetVersion() throws Exception {
-		String folder = "TestNesting";
+		String folder = "TestNesting/first";
 		String actualName = "Actual.java";
 		String expectedName = "Expected1.16.1.txt";
 		String targetVersion = "1.16.1";
@@ -61,7 +60,7 @@ class TestVersionNesting extends TestBase {
 	 */
 	@Test
 	void testTargetAndNestingVersion() throws Exception {
-		String folder = "TestNesting";
+		String folder = "TestNesting/first";
 		String actualName = "Actual.java";
 		String expectedName = "Expected1.16.5.txt";
 		String targetVersion = "1.16.5";
@@ -76,4 +75,137 @@ class TestVersionNesting extends TestBase {
 		assertEquals(expected, actual);
 	}
 	
+	/**
+	 * TargetVersion: 1.17.1
+	 * Expected: 1.16.1 and 1.16.5
+	 * @throws Exception
+	 */
+	@Test
+	void testTargetAndNestingVersionAbove() throws Exception {
+		String folder = "TestNesting/first";
+		String actualName = "Actual.java";
+		String expectedName = "Expected1.16.5.txt";
+		String targetVersion = "1.17.1";
+		
+		Pair<List<String>, List<String>> lines = getLines(folder, actualName, expectedName);
+		
+		List<String> linesActual = processor.preprocess(targetVersion, lines.left(), "Actual", FileNameUtils.getExtension(actualName));
+		
+		String actual = String.join("\n", linesActual);
+		String expected = String.join("\n", lines.right());
+		
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * TargetVersion: 1.14.4
+	 * Expected: 1.14.4
+	 * @throws Exception
+	 */
+	@Test
+	void testTargetAndNestingDefault() throws Exception {
+		String folder = "TestNesting/first";
+		String actualName = "Actual.java";
+		String expectedName = "Expected1.14.4.txt";
+		String targetVersion = "1.14.4";
+		
+		Pair<List<String>, List<String>> lines = getLines(folder, actualName, expectedName);
+		
+		List<String> linesActual = processor.preprocess(targetVersion, lines.left(), "Actual", FileNameUtils.getExtension(actualName));
+		
+		String actual = String.join("\n", linesActual);
+		String expected = String.join("\n", lines.right());
+		
+		assertEquals(expected, actual);
+	}
+	
+	// =======================================================
+	
+	/**
+	 * TargetVersion: 1.16.1
+	 * Expected: 1.16.1 only
+	 * @throws Exception
+	 */
+	@Test
+	void testTripleNesting() throws Exception {
+		String folder = "TestNesting/second";
+		String actualName = "Actual.java";
+		String expectedName = "Expected1.16.1.txt";
+		String targetVersion = "1.16.1";
+		
+		Pair<List<String>, List<String>> lines = getLines(folder, actualName, expectedName);
+		
+		List<String> linesActual = processor.preprocess(targetVersion, lines.left(), "Actual", FileNameUtils.getExtension(actualName));
+		
+		String actual = String.join("\n", linesActual);
+		String expected = String.join("\n", lines.right());
+		
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * TargetVersion: 1.16.5
+	 * Expected: 1.16.1, 1.16.5
+	 * @throws Exception
+	 */
+	@Test
+	void testTripleNesting2() throws Exception {
+		String folder = "TestNesting/second";
+		String actualName = "Actual.java";
+		String expectedName = "Expected1.16.5.txt";
+		String targetVersion = "1.16.5";
+		
+		Pair<List<String>, List<String>> lines = getLines(folder, actualName, expectedName);
+		
+		List<String> linesActual = processor.preprocess(targetVersion, lines.left(), "Actual", FileNameUtils.getExtension(actualName));
+		
+		String actual = String.join("\n", linesActual);
+		String expected = String.join("\n", lines.right());
+		
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * TargetVersion: 1.17.1
+	 * Expected: 1.16.1, 1.16.5, 1.17.1
+	 * @throws Exception
+	 */
+	@Test
+	void testTripleNesting3() throws Exception {
+		String folder = "TestNesting/second";
+		String actualName = "Actual.java";
+		String expectedName = "Expected1.17.1.txt";
+		String targetVersion = "1.17.1";
+		
+		Pair<List<String>, List<String>> lines = getLines(folder, actualName, expectedName);
+		
+		List<String> linesActual = processor.preprocess(targetVersion, lines.left(), "Actual", FileNameUtils.getExtension(actualName));
+		
+		String actual = String.join("\n", linesActual);
+		String expected = String.join("\n", lines.right());
+		
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * TargetVersion: 1.18.2
+	 * Expected: 1.16.1, 1.16.5, 1.18.2
+	 * @throws Exception
+	 */
+	@Test
+	void testTripleNesting4() throws Exception {
+		String folder = "TestNesting/second";
+		String actualName = "Actual.java";
+		String expectedName = "Expected1.18.2.txt";
+		String targetVersion = "1.18.2";
+		
+		Pair<List<String>, List<String>> lines = getLines(folder, actualName, expectedName);
+		
+		List<String> linesActual = processor.preprocess(targetVersion, lines.left(), "Actual", FileNameUtils.getExtension(actualName));
+		
+		String actual = String.join("\n", linesActual);
+		String expected = String.join("\n", lines.right());
+		
+		assertEquals(expected, actual);
+	}
 }
