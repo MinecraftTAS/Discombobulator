a5cc9d7daeb613bb74e59849f9a090af142e3654
Scribble <scribble@minecrafttas.com>
2025-02-19 21:38:23 +0100

    Update to java 23

--- src/main/java/com/minecrafttas/discombobulator/tasks/TaskPreprocessVersion.java
+++ src/main/java/com/minecrafttas/discombobulator/tasks/TaskPreprocessVersion.java
@@ -3,6 +3,11 @@ package com.minecrafttas.discombobulator.tasks;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
 
+/**
+ * Task for preprocessing one version into all other versions including the base source
+ * 
+ * @author Scribble
+ */
 public class TaskPreprocessVersion extends DefaultTask {
 
 	@TaskAction
